6542f19fe2dae51605d8b7aea5cc9cbc5fbd977d
ivanori1 <coric.ivan@yandex.com>
2023-03-28 16:41:17 +0200

    remove commented code

--- src/Orderbook.js
+++ src/Orderbook.js
@@ -10,13 +10,13 @@ const Orderbook = () => {
   const dispatch = useDispatch();
   const bids = useSelector((state) => state.orderbook.bids);
   const asks = useSelector((state) => state.orderbook.asks);
-  
+
   // Define spread state at the top level of your component
   const [spread, setSpread] = useState(null);
 
   useEffect(() => {
     const socket = new W3CWebSocket('wss://api-pub.bitfinex.com/ws/2');
-    
+
     socket.addEventListener('open', () => {
       console.log('WebSocket open');
       const msg = JSON.stringify({
@@ -31,39 +31,20 @@ const Orderbook = () => {
 
     socket.addEventListener('message', (message) => {
       const data = JSON.parse(message.data);
-     // console.log(Array.isArray(data))
-      //console.log(data.length)
-      //console.log(Array.isArray(data))
+
       if (Array.isArray(data) && data.length === 2) {
-        if(data[1] ==='hb') {
+        if (data[1] === 'hb') {
           return
         }
-        if(data[1].length === 50) {
+        if (data[1].length === 50) {
           const bids = data[1].slice(0, 25);
-          const asks = data[1].slice(25, 50);
-          console.log(bids)
-          console.log(asks)
-          
+          const asks = data[1].slice(25, 50)
           dispatch(updateBids(bids));
           dispatch(updateAsks(asks));
         }
-        
-        // // Update the spread state
-        // const bestBid = bids[0]?.[0];
-        // const bestAsk = asks[0]?.[0];
-        // if (bestBid && bestAsk) {
-        //   const spread = (bestAsk - bestBid).toFixed(2);
-        //   setSpread(spread);
-        // } else {
-        //   setSpread(null);
-        // }
       }
     });
 
-    // Return a cleanup function to close the WebSocket connection
-    // return () => {
-    //   socket.close();
-    // };
   }, [dispatch]);
 
   // Render the bids and asks
@@ -81,30 +62,30 @@ const Orderbook = () => {
     <div >
       <h1>Orderbook {symbol}</h1>
       <div className='table-container'>
-      <table className='bids-table'>
-        <thead >
-          <tr>
-            <th>Count</th>
-            <th>Amount</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {renderRows(bids)}
-        </tbody>
-      </table>
-      <table className='asks-table'>
-        <thead>
-          <tr>
-            <th>Count</th>
-            <th>Amount</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {renderRows(asks)}
-        </tbody>
-      </table>
+        <table className='bids-table'>
+          <thead >
+            <tr>
+              <th>Count</th>
+              <th>Amount</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {renderRows(bids)}
+          </tbody>
+        </table>
+        <table className='asks-table'>
+          <thead>
+            <tr>
+              <th>Count</th>
+              <th>Amount</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {renderRows(asks)}
+          </tbody>
+        </table>
       </div>
       {spread && <p>Spread: {spread}</p>}
     </div>
